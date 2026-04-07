4537685beb4455b5b2aa875f2d60422878a8bc3a
Jacek Galowicz <jacek@galowicz.de>
2026-04-07 17:43:56 +0200

    add browser test annotation

--- docs/tutorials/graphical-vm-tests.md
+++ docs/tutorials/graphical-vm-tests.md
@@ -1,5 +1,25 @@
 # Graphical VMs and OCR
 
+Let's test something with [optical character recognition](https://en.wikipedia.org/wiki/Optical_character_recognition) (OCR) in a graphical VM.
+
+In this scenario, we define a machine `server` that serves the standard [Apache HTTP server](https://httpd.apache.org/) _"It works!"_ page.
+On the second machine, `client`, we run [Mozilla Firefox](https://www.firefox.com) to display this page and test if the text is really visible on the graphical desktop.
+
 ```nix title="browser.nix"
 --8<-- "examples/browser.nix"
 ```
+
+1.  **Enabling OCR**
+
+    This setting adds `tesseract` and `imagemagick` to the test driver closure.
+    It is not enabled by default to reduce the closure for non-graphical tests, which are the majority.
+
+2.  **Configuring the desktop**
+
+    This profile is commonly used among graphical tests in nixpkgs and configures a small desktop environment with auto login.
+
+    Importing this file is not mandatory - we can always configure this ourselves.
+
+3.  **Resolution settings**
+
+    We reduce the display resolution to result in fewer pixels, which in turn reduces the resource usage of the Tesseract OCR analysis.
--- examples/browser.nix
+++ examples/browser.nix
@@ -1,17 +1,12 @@
-{ lib, ... }:
+{ pkgs, ... }:
+let
+  nixpkgs = pkgs.path;
+in
 {
-  name = "nixcon-browser-test";
+  name = "Browser test";
   globalTimeout = 500;
 
-  enableOCR = true;
-
-  defaults = {
-    networking.firewall.enable = false;
-    virtualisation.resolution = {
-      x = 800;
-      y = 600;
-    };
-  };
+  enableOCR = true; # (1)
 
   nodes = {
     server = {
@@ -22,7 +17,7 @@
       { pkgs, ... }:
       {
         imports = [
-          (pkgs.path + "/nixos/tests/common/x11.nix")
+          (nixpkgs + "/nixos/tests/common/x11.nix") # (2)
         ];
 
         programs.firefox.enable = true;
@@ -30,57 +25,44 @@
         environment.systemPackages = [
           pkgs.xdotool
         ];
+
+        virtualisation.resolution = {
+          # (3)
+          x = 800;
+          y = 600;
+        };
       };
   };
 
-  interactive.nodes = {
-    server = {
-      virtualisation.forwardPorts = [
-        {
-          from = "host";
-          host.port = 8000;
-          guest.port = 80;
-        }
-      ];
-    };
+  testScript = ''
+    start_all()
 
-    client = import ../debug-host-module.nix;
-  };
+    client.systemctl("start network-online.target")
+    server.systemctl("start network-online.target")
+    client.wait_for_unit("network-online.target")
+    server.wait_for_unit("network-online.target")
 
-  testScript =
-    { nodes, ... }:
-    let
-      firefox = lib.getExe nodes.client.programs.firefox.package;
-    in
-    ''
-      start_all()
+    server.succeed("ping -c 1 client")
+    client.succeed("ping -c 1 server")
 
-      for m in [ client, server ]:
-        m.systemctl("start network-online.target")
-        m.wait_for_unit("network-online.target")
+    client.wait_for_x()
 
-      server.succeed("ping -c 1 client")
-      client.succeed("ping -c 1 server")
+    with subtest("open and close firefox"):
+      client.succeed("xterm -e 'firefox about:welcome' >&2 &")
+      client.wait_for_window("Firefox")
+      client.sleep(5)
+      client.succeed("xdotool key ctrl+q")
+      client.wait_for_text(".uit .irefox")
+      client.succeed("xdotool key space")
+      client.sleep(2)
 
-      client.wait_for_x()
+    with subtest("open website on server"):
+      client.succeed("xterm -e 'firefox http://server' >&2 &")
+      client.wait_for_window("Firefox")
+      client.sleep(2)
+      client.screenshot("it-works")
 
-      with subtest("open and close firefox"):
-        client.succeed("xterm -e '${firefox} about:welcome' >&2 &")
-        client.wait_for_window("Firefox")
-        client.sleep(5)
-        client.succeed("xdotool key ctrl+q")
-        client.wait_for_text(".uit .irefox")
-        client.succeed("xdotool key space")
-        client.sleep(5)
-
-      with subtest("open website on server"):
-        client.succeed("xterm -e '${firefox} http://server' >&2 &")
-        client.wait_for_window("Firefox")
-        client.sleep(5)
-
-        client.screenshot("it-works")
-
-        screen_content = client.get_screen_text()
-        assert "It works!" in screen_content, "It works! page is on screen"
-    '';
+      screen_content = client.get_screen_text()
+      t.assertIn("It works!", screen_content, "It works! page is on screen")
+  '';
 }
