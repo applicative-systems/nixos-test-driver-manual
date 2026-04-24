0f1bf73c27a125ce4f9ecf0eb1f23d1246f4c638
Jacek Galowicz <jacek@applicative.systems>
2026-04-24 16:20:42 +0100

    Update flake inputs to include latest changes on test driver

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     mkdocs-flake.url = "github:applicative-systems/mkdocs-flake";
 
